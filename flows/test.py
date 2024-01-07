11851fbdbb684062fc9ce2988d60c3cba2ada78b
Roman <roman.smirnov@vicuesoft.com>
2024-01-07 20:51:11 +0400

    debug commit 2

---
+++ flows/test.py
@@ -0,0 +1,14 @@
+import sys
+import os
+from prefect import flow, task
+from prefect.context import FlowRunContext
+from dotenv import dotenv_values
+from prefect.blocks.system import JSON
+import json
+
+@flow(log_prints=True)
+def test(items, params):
+    print('tessssssssssst')
+
+if __name__ == "__main__":
+    pass
